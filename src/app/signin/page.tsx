d5587dac3d2d0a526edfd49144f77c1ff63ac29e
Austin Ameh <arewageek@gmail.com>
2025-05-17 17:18:27 +0100

    First commit

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,19 @@
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
+import React from 'react'
+
+const SigninPage = () => {
+    return (
+        <div className='h-screen w-screen flex items-center justify-center'>
+            <Card className='w-full lg:w-1/2'>
+                <CardHeader>
+                    Let's get you signed in
+                </CardHeader>
+                <CardContent>
+
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
+
+export default SigninPage
